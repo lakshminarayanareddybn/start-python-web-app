b91efe4d42ef219d61dacef2918f4ef48ed923e6
Lakshminarayana reddy <lakshminarayana84.reddy@gmail.com>
2023-07-16 11:34:17 +0000

    added code

--- streamlit_001.py
+++ streamlit_001.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import requests
+import json
 
 st.title("Hello Streamlit")
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+data ={
+    "operation":"+",
+    "values":[
+        3,
+        4,
+        5,
+        6
+    ]
+}
+response = requests.post("http://127.0.0.1:8082/math", data=json.dumps(data), headers=headers, timeout=20)
+print(response.content)
+st.write(response.content)
